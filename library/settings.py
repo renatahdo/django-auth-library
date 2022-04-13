62d7c0d7966bf45c04d7441ac9431f1b6c732104
Renata Hurtado <renatah2198@gmail.com>
2022-04-13 17:38:39 +0000

    First workflow

--- library/settings.py
+++ library/settings.py
@@ -161,7 +161,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=int(env.get('JWT_TOKEN_TIME'))),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=120),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ALGORITHM': 'HS256',
     'AUTH_HEADER_TYPES': ('Bearer',),
